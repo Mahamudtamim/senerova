e42de9d230b6a2672bfce6dbefd3838ca5bcf5dd
Mahamudul Hasan Tamim <mahamudulhasantamim0@gmail.com>
2026-04-18 13:28:52 +0300

    Update tailwind-config.js

--- js/tailwind-config.js
+++ js/tailwind-config.js
@@ -1,1 +1,2 @@
+tailwind.config={theme:{extend:{fontFamily:{heading:['Space Grotesk','sans-serif'],body:['Inter','sans-serif']},colors:{blush:'#E8B4B8',cream:'#FFF8F0','cream-dark':'#F5EDE8','cream-bg':'#FBF7F4','rose-gold':'#B76E79','rose-dark':'#9A5A63','brand-text':'#2D2A2A','brand-muted':'#8B7E7E','brand-faint':'#C4B5B5'}}}}
 
